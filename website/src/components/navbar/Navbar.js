20e52e615b21a61286b2b37ef2f6ce0a03ecff01
Christian Rudder <ringthebellpepper@gmail.com>
2022-03-13 16:17:26 -0400

    mobile menu view

--- website/src/components/navbar/Navbar.js
+++ website/src/components/navbar/Navbar.js
@@ -1,12 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {BsFillCloudHaze2Fill} from 'react-icons/bs';
+import {BsFillCloudHaze2Fill, } from 'react-icons/bs';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {HiX} from 'react-icons/hi'
 import {Link} from 'react-router-dom';
 import NavLink from './NavLink';
+
+
 import './Navbar.css'
 
 
 function Navbar() {
 
+    const [isClicked, setisClicked] = useState(false); //is the hamburger clicked?
+    
+    const handleClick = () => setisClicked(!isClicked);
+    const closeMobileMenu = () => setisClicked(false);
 
 
   return (
@@ -16,17 +24,28 @@ function Navbar() {
 
                  {/* Here we want to put our logo button that routes back to the homepage */}
                 <Link to='/' className='logo-btn'>
-                    <BsFillCloudHaze2Fill className='logo-icon'/>crub-hub 
+
+                    <BsFillCloudHaze2Fill className='logo-icon'/>
+                    crub-hub 
+
                 </Link>
 
                 {/* Hamburger Menu Icon */}
-                <div className='menu-icon'></div>
+                <div className='menu-icon' onClick={handleClick}>
+
+                    {!isClicked ? <GiHamburgerMenu /> : <HiX />}
+
+                </div>
                 {/* unordered list of all the nav links */}
-                <ul className='nav-menu'>
-                    <NavLink path='/' label='Editor'/>
-                    <NavLink path='/' label='Web Portfolio'/>
-                    <NavLink path='/' label='Art'/>
-                    <NavLink path='/' label='About'/>
+                <ul 
+                    className={ isClicked? 'nav-menu active' : 'nav-menu' }
+                >
+
+                    <NavLink path='/' label='Editor' onClick={closeMobileMenu}/>
+                    <NavLink path='/' label='Web Portfolio' onClick={closeMobileMenu}/>
+                    <NavLink path='/' label='Art' onClick={closeMobileMenu}/>
+                    <NavLink path='/' label='About' onClick={closeMobileMenu}/>
+
                 </ul>
             </div>
         </nav>
